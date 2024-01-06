bdeaff0f3f378749ade11d1470730eb60ce382a4
serhatblb <serhatblb23@gmail.com>
2024-01-06 21:59:40 +0300

    Typescript Control Flow Statements added

--- app.js
+++ app.js
@@ -1,156 +1,243 @@
 ///////////////////////////////////////////////////
 //TypeScript - Data Types
 ///////////////////////////////////////////////////
-var age = 29;
-var firstname = "Serhat";
-var lastname = "Bulbul";
-var isUpdated = true;
-console.log(firstname, lastname);
-function display(id, name) {
-    console.log("Id= " + id + ", Name = " + name);
-}
-/////////////////////////////////////////////
-// Data Types - Number - String - Boolean
-var first = 123; //number
-var second = 0x37CF; //hexadecimal
-var third = 255; //octal
-var fourth = 33288; //binary
-console.log(first);
-console.log(second);
-console.log(third);
-console.log(fourth);
-var employeeFirstName = "Serhat Bulbul";
-console.log(employeeFirstName);
-//////////////////////////////////////////////////
-var employeeName = "Serhat Bulbul";
-var employeeDept = "Finance";
-//Pre-ES6
-var employeDesc1 = employeeName + " works in the " + employeeDept + " department.";
-console.log(employeDesc1);
-//Post-ES6
-var employeDesc2 = "".concat(employeeName, " works in the ").concat(employeeDept, " department.");
-console.log(employeDesc2);
-var isPresent = true;
-console.log(isPresent);
-/////////////////////////////////////////////
-// Data Types - Arrays
-var firstNames = ['Serhat', 'Ali', 'Veli']; // first Method
-console.log(firstNames);
-var firstNames2 = ['Serhat', 'Ali', 'Veli2']; // second Method
-console.log(firstNames2);
-var arr = [1, 2, 3, 'Serhat', 'Bulbul', true, false];
-console.log(arr);
-var fruits;
-fruits = ['Banana', 'Apple'];
-console.log(fruits);
-var ids;
-ids = [122, 133, 144, 155];
-console.log(ids);
-var values = ['Apple', 2, 'Orange', 3, 'Banana'];
-var values2 = ['Serhat', 'Ali', 'Veli2', 23, 33]; // second 
-console.log(fruits[1]);
-/////////////////////////////////////////////
-// Data Types - Tuple
-var Id = 1;
-var instructorName = 'Serhat';
-var instructor = [1, "Serhat"];
-var user;
-user = [1, "Steve", true, 20, "Admin"];
-console.log(instructor, user);
-var employee33 = [
-    [1, "Steve"],
-    [2, "Bill"],
-    [3, "Jeff"]
-];
-console.log(employee33);
-var instructor2;
-instructor2 = {
-    firstName: 'serhat',
-    lastName: 'Bulbul',
-    age: 28,
-    jobTitle: 'Web Developer'
-};
-console.log(instructor2.age);
-/////////////////////////////////////////////
-// Data Types - Enum
-var Media;
-(function (Media) {
-    Media[Media["Newspaper"] = 10] = "Newspaper";
-    Media[Media["Newsletter"] = 11] = "Newsletter";
-    Media[Media["Magazine"] = 12] = "Magazine";
-    Media[Media["Book"] = 13] = "Book";
-})(Media || (Media = {}));
-console.log(Media.Magazine); //baslangic degerinden +1 koyarak sayıyor ve yazdırıyor
-console.log(Media[13]); // --> Book
-var MediaP;
-(function (MediaP) {
-    MediaP["Newspaper"] = "NEWSPAPER";
-    MediaP["Newsletter"] = "NEWSLETTER";
-    MediaP["Magazine"] = "MAGAZINE";
-    MediaP["Book"] = "BOOK";
-})(MediaP || (MediaP = {}));
-console.log(MediaP.Newsletter); // --> NEWSLETTER
-console.log(MediaP['Newspaper']); // --> NEWSPAPER
-/////////////////////////////////////////////
-// Data Types - Union
-var codeee = "123";
-console.log(codeee);
-/////////////////////////////////////////////
-// Data Types - Any
-var someThing = "Hello";
-console.log(someThing);
-someThing = 33;
-console.log(someThing);
-someThing = {
-    firstName: "Serhat",
-    lastName: "Bulbul"
-};
-console.log(typeof (someThing));
-var arr22 = ["Serhat", 222, true];
-console.log(arr22);
-/////////////////////////////////////////////
-// Data Types - Void
-function sayHello() {
-    console.log("Hello");
-}
-sayHello();
-/////////////////////////////////////////////
-// // Data Types - Never
-// function throwError(errorMsg: string): never {
-//     throw new Error(errorMsg);
+// let age: number = 29;
+// let firstname: string = "Serhat";
+// let lastname: string = "Bulbul";
+// let isUpdated: boolean = true;
+// console.log(firstname, lastname)
+// function display(id: any, name: string) {
+//     console.log("Id= " + id + ", Name = " + name);
 // }
-// throwError("Something went wrong serhat");
-// let someThing3: void = undefined; //null ve undefined deger atanıyor
-// // let nothing3: never = undefined; // null ve undefined bile  degeri atanamıyor
-// console.log(someThing3)
-// // console.log(nothing3)
-/////////////////////////////////////////////
-// Data Types - Inference
-var sayac = 0;
-console.log(typeof (sayac));
-// function increment(counter: number){
+// /////////////////////////////////////////////
+// // Data Types - Number - String - Boolean
+// let first: number = 123; //number
+// let second: number = 0x37CF; //hexadecimal
+// let third: number = 0o377; //octal
+// let fourth: number = 0o101010; //binary
+// console.log(first);
+// console.log(second);
+// console.log(third);
+// console.log(fourth);
+// let employeeFirstName: string = "Serhat Bulbul";
+// console.log(employeeFirstName);
+// //////////////////////////////////////////////////
+// let employeeName: string = "Serhat Bulbul";
+// let employeeDept: string = "Finance";
+// //Pre-ES6
+// let employeDesc1: string = employeeName + " works in the " + employeeDept + " department.";
+// console.log(employeDesc1)
+// //Post-ES6
+// let employeDesc2: string = `${employeeName} works in the ${employeeDept} department.`;
+// console.log(employeDesc2);
+// let isPresent: boolean = true;
+// console.log(isPresent);
+// /////////////////////////////////////////////
+// // Data Types - Arrays
+// let firstNames: string[] = ['Serhat', 'Ali', 'Veli']; // first Method
+// console.log(firstNames);
+// let firstNames2: Array < string > = ['Serhat', 'Ali', 'Veli2']; // second Method
+// console.log(firstNames2);
+// let arr = [1, 2, 3, 'Serhat', 'Bulbul', true, false];
+// console.log(arr);
+// let fruits: Array < string > ;
+// fruits = ['Banana', 'Apple'];
+// console.log(fruits);
+// let ids: Array < number > ;
+// ids = [122, 133, 144, 155];
+// console.log(ids);
+// let values: (string | number)[] = ['Apple', 2, 'Orange', 3, 'Banana']
+// let values2: Array < string | number > = ['Serhat', 'Ali', 'Veli2', 23, 33]; // second 
+// console.log(fruits[1]);
+// /////////////////////////////////////////////
+// // Data Types - Tuple
+// let Id: number = 1;
+// let instructorName: string = 'Serhat';
+// let instructor: [number, string] = [1, "Serhat"];
+// var user: [number, string, boolean, number, string];
+// user = [1, "Steve", true, 20, "Admin"]
+// console.log(instructor, user);
+// let employee33 = [
+//     [1, "Steve"],
+//     [2, "Bill"],
+//     [3, "Jeff"]
+// ];
+// console.log(employee33)
+// /////////////////////////////////////////////
+// // Data Types - Object
+// type Person = {
+//     firstName: string,
+//     lastName: string,
+//     age: number,
+//     jobTitle: string
+// }
+// let instructor2: Person;
+// instructor2 = {
+//     firstName: 'serhat',
+//     lastName: 'Bulbul',
+//     age: 28,
+//     jobTitle: 'Web Developer'
+// }
+// console.log(instructor2.age);
+// /////////////////////////////////////////////
+// // Data Types - Enum
+// enum Media {
+//     Newspaper = 10,
+//         Newsletter,
+//         Magazine,
+//         Book
+// }
+// console.log(Media.Magazine) //baslangic degerinden +1 koyarak sayıyor ve yazdırıyor
+// console.log(Media[13]); // --> Book
+// enum MediaP {
+//     Newspaper = "NEWSPAPER",
+//         Newsletter = "NEWSLETTER",
+//         Magazine = "MAGAZINE",
+//         Book = "BOOK"
+// }
+// console.log(MediaP.Newsletter); // --> NEWSLETTER
+// console.log(MediaP['Newspaper']); // --> NEWSPAPER
+// /////////////////////////////////////////////
+// // Data Types - Union
+// let codeee: string | number = "123";
+// console.log(codeee);
+// /////////////////////////////////////////////
+// // Data Types - Any
+// let someThing: any = "Hello";
+// console.log(someThing);
+// someThing = 33;
+// console.log(someThing);
+// someThing = {
+//     firstName: "Serhat",
+//     lastName: "Bulbul"
+// }
+// console.log(typeof (someThing));
+// let arr22: any[] = ["Serhat", 222, true];
+// console.log(arr22)
+// /////////////////////////////////////////////
+// // Data Types - Void
+// function sayHello(): void {
+//     console.log("Hello");
+// }
+// sayHello();
+// /////////////////////////////////////////////
+// // // Data Types - Never
+// // function throwError(errorMsg: string): never {
+// //     throw new Error(errorMsg);
+// // }
+// // throwError("Something went wrong serhat");
+// // let someThing3: void = undefined; //null ve undefined deger atanıyor
+// // // let nothing3: never = undefined; // null ve undefined bile  degeri atanamıyor
+// // console.log(someThing3)
+// // // console.log(nothing3)
+// /////////////////////////////////////////////
+// // Data Types - Inference
+// let sayac = 0;
+// console.log(typeof (sayac));
+// // function increment(counter: number){
+// //     return counter++;
+// // }
+// function increment(counter: number): number { // geriye donus tipi de number olşdu
 //     return counter++;
 // }
-function increment(counter) {
-    return counter++;
-}
-var a = "some text";
-var b = 123;
-//a = b; //app.ts:196:1 - error TS2322: Type 'number' is not assignable to type 'string'
-function sum(a, b) {
-    return a + b;
-}
-var total = sum(10, 12);
-// let total: string = sum(10, 12); //app.ts:202:5 - error TS2322: Type 'number' is not assignable to type 'string'.
-/////////////////////////////////////////////
-// Data Types - Assertion
-var code = 123;
-var empCode = code;
-console.log(typeof (empCode));
-var employee12 = {};
-console.log(typeof (employee12));
-employee12.name = "Serhat";
-console.log(employee12);
+// let a = "some text";
+// let b = 123;
+// //a = b; //app.ts:196:1 - error TS2322: Type 'number' is not assignable to type 'string'
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+// let total: number = sum(10, 12);
+// // let total: string = sum(10, 12); //app.ts:202:5 - error TS2322: Type 'number' is not assignable to type 'string'.
+// /////////////////////////////////////////////
+// // Data Types - Assertion
+// let code: any = 123;
+// let empCode = < number > code;
+// console.log(typeof (empCode));
+// interface Employee {
+//     name: string;
+//     code: number;
+// }
+// let employee12 = < Employee > {}
+// console.log(typeof (employee12));
+// employee12.name = "Serhat";
+// console.log(employee12);
 ///////////////////////////////////////////////////
 //TypeScript - Control Flow Statements
 ///////////////////////////////////////////////////
+// 1)  If Else Ternary Operator
+var x = 35;
+var y = 25;
+if (x > y) {
+    console.log("".concat(x, " buyuktur ").concat(y));
+}
+else if (x < y) {
+    console.log("".concat(x, " kucuktur ").concat(y));
+}
+else {
+    console.log("".concat(x, " esit ").concat(y));
+}
+;
+(x > y) ? console.log("".concat(x, " buyuktur ").concat(y)) : console.log("".concat(x, " kucuk veya esit ").concat(y));
+// 2)  Switch Case
+var day = 5;
+switch (day) {
+    case 0:
+        console.log("Pazar");
+        break;
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+    case 4:
+        console.log("Perşembe");
+        break;
+    case 5:
+        console.log("Cuma");
+        break;
+    case 6:
+        console.log("Cumartesi");
+        break;
+    default:
+        console.log("1-7 arasinda sayi gir");
+        break;
+}
+// 2)  For Loop
+for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        for (var k = 0; k < 3; k++) {
+            console.log(i, j, k);
+        }
+    }
+}
+var arr = [10, 20, 30, 40, 50];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) { // for of
+    var item = arr_1[_i];
+    console.log(item);
+}
+var str = "Serhat Bülbül";
+for (var _a = 0, str_1 = str; _a < str_1.length; _a++) {
+    var char = str_1[_a];
+    console.log(char);
+}
+for (var index in str) { // for in yapisi index degerini veriyor
+    console.log(index);
+}
+// 3) While Loop
+var counter = 5;
+while (counter < 5) {
+    console.log(counter);
+    counter++;
+    if (counter === 3) {
+        break;
+    }
+}
+// 4) Do While Loop
+do {
+    console.log(counter);
+    counter++;
+} while (counter < 5);
--- app.ts
+++ app.ts
@@ -1,229 +1,320 @@
 ///////////////////////////////////////////////////
 //TypeScript - Data Types
 ///////////////////////////////////////////////////
-let age: number = 29;
-let firstname: string = "Serhat";
-let lastname: string = "Bulbul";
+// let age: number = 29;
+// let firstname: string = "Serhat";
+// let lastname: string = "Bulbul";
 
-let isUpdated: boolean = true;
-console.log(firstname, lastname)
+// let isUpdated: boolean = true;
+// console.log(firstname, lastname)
 
-function display(id: any, name: string) {
-    console.log("Id= " + id + ", Name = " + name);
-}
-/////////////////////////////////////////////
-// Data Types - Number - String - Boolean
+// function display(id: any, name: string) {
+//     console.log("Id= " + id + ", Name = " + name);
+// }
+// /////////////////////////////////////////////
+// // Data Types - Number - String - Boolean
 
-let first: number = 123; //number
-let second: number = 0x37CF; //hexadecimal
-let third: number = 0o377; //octal
-let fourth: number = 0o101010; //binary
+// let first: number = 123; //number
+// let second: number = 0x37CF; //hexadecimal
+// let third: number = 0o377; //octal
+// let fourth: number = 0o101010; //binary
 
-console.log(first);
-console.log(second);
-console.log(third);
-console.log(fourth);
+// console.log(first);
+// console.log(second);
+// console.log(third);
+// console.log(fourth);
 
-let employeeFirstName: string = "Serhat Bulbul";
+// let employeeFirstName: string = "Serhat Bulbul";
 
-console.log(employeeFirstName);
-//////////////////////////////////////////////////
-let employeeName: string = "Serhat Bulbul";
-let employeeDept: string = "Finance";
+// console.log(employeeFirstName);
+// //////////////////////////////////////////////////
+// let employeeName: string = "Serhat Bulbul";
+// let employeeDept: string = "Finance";
 
-//Pre-ES6
-let employeDesc1: string = employeeName + " works in the " + employeeDept + " department.";
-console.log(employeDesc1)
+// //Pre-ES6
+// let employeDesc1: string = employeeName + " works in the " + employeeDept + " department.";
+// console.log(employeDesc1)
 
-//Post-ES6
-let employeDesc2: string = `${employeeName} works in the ${employeeDept} department.`;
-console.log(employeDesc2);
+// //Post-ES6
+// let employeDesc2: string = `${employeeName} works in the ${employeeDept} department.`;
+// console.log(employeDesc2);
 
-let isPresent: boolean = true;
-console.log(isPresent);
+// let isPresent: boolean = true;
+// console.log(isPresent);
 
-/////////////////////////////////////////////
-// Data Types - Arrays
+// /////////////////////////////////////////////
+// // Data Types - Arrays
 
-let firstNames: string[] = ['Serhat', 'Ali', 'Veli']; // first Method
-console.log(firstNames);
-let firstNames2: Array < string > = ['Serhat', 'Ali', 'Veli2']; // second Method
-console.log(firstNames2);
+// let firstNames: string[] = ['Serhat', 'Ali', 'Veli']; // first Method
+// console.log(firstNames);
+// let firstNames2: Array < string > = ['Serhat', 'Ali', 'Veli2']; // second Method
+// console.log(firstNames2);
 
-let arr = [1, 2, 3, 'Serhat', 'Bulbul', true, false];
-console.log(arr);
+// let arr = [1, 2, 3, 'Serhat', 'Bulbul', true, false];
+// console.log(arr);
 
-let fruits: Array < string > ;
-fruits = ['Banana', 'Apple'];
-console.log(fruits);
+// let fruits: Array < string > ;
+// fruits = ['Banana', 'Apple'];
+// console.log(fruits);
 
-let ids: Array < number > ;
-ids = [122, 133, 144, 155];
-console.log(ids);
+// let ids: Array < number > ;
+// ids = [122, 133, 144, 155];
+// console.log(ids);
 
-let values: (string | number)[] = ['Apple', 2, 'Orange', 3, 'Banana']
-let values2: Array < string | number > = ['Serhat', 'Ali', 'Veli2', 23, 33]; // second 
+// let values: (string | number)[] = ['Apple', 2, 'Orange', 3, 'Banana']
+// let values2: Array < string | number > = ['Serhat', 'Ali', 'Veli2', 23, 33]; // second 
 
-console.log(fruits[1]);
+// console.log(fruits[1]);
 
-/////////////////////////////////////////////
-// Data Types - Tuple
+// /////////////////////////////////////////////
+// // Data Types - Tuple
 
-let Id: number = 1;
-let instructorName: string = 'Serhat';
+// let Id: number = 1;
+// let instructorName: string = 'Serhat';
 
-let instructor: [number, string] = [1, "Serhat"];
+// let instructor: [number, string] = [1, "Serhat"];
 
-var user: [number, string, boolean, number, string];
-user = [1, "Steve", true, 20, "Admin"]
+// var user: [number, string, boolean, number, string];
+// user = [1, "Steve", true, 20, "Admin"]
 
-console.log(instructor, user);
+// console.log(instructor, user);
 
-let employee33 = [
-    [1, "Steve"],
-    [2, "Bill"],
-    [3, "Jeff"]
-];
-console.log(employee33)
+// let employee33 = [
+//     [1, "Steve"],
+//     [2, "Bill"],
+//     [3, "Jeff"]
+// ];
+// console.log(employee33)
 
-/////////////////////////////////////////////
-// Data Types - Object
+// /////////////////////////////////////////////
+// // Data Types - Object
 
-type Person = {
-    firstName: string,
-    lastName: string,
-    age: number,
-    jobTitle: string
-}
-
-let instructor2: Person;
-instructor2 = {
-    firstName: 'serhat',
-    lastName: 'Bulbul',
-    age: 28,
-    jobTitle: 'Web Developer'
-}
-
-console.log(instructor2.age);
-
-/////////////////////////////////////////////
-// Data Types - Enum
-
-enum Media {
-    Newspaper = 10,
-        Newsletter,
-        Magazine,
-        Book
-}
-console.log(Media.Magazine) //baslangic degerinden +1 koyarak sayıyor ve yazdırıyor
-console.log(Media[13]); // --> Book
-
-enum MediaP {
-    Newspaper = "NEWSPAPER",
-        Newsletter = "NEWSLETTER",
-        Magazine = "MAGAZINE",
-        Book = "BOOK"
-}
-
-console.log(MediaP.Newsletter); // --> NEWSLETTER
-console.log(MediaP['Newspaper']); // --> NEWSPAPER
-
-/////////////////////////////////////////////
-// Data Types - Union
-
-let codeee: string | number = "123";
-console.log(codeee);
-
-/////////////////////////////////////////////
-// Data Types - Any
-
-let someThing: any = "Hello";
-console.log(someThing);
-someThing = 33;
-console.log(someThing);
-someThing = {
-    firstName: "Serhat",
-    lastName: "Bulbul"
-}
-console.log(typeof (someThing));
-
-let arr22: any[] = ["Serhat", 222, true];
-console.log(arr22)
-
-
-/////////////////////////////////////////////
-// Data Types - Void
-
-function sayHello(): void {
-    console.log("Hello");
-}
-
-sayHello();
-
-/////////////////////////////////////////////
-// // Data Types - Never
-
-// function throwError(errorMsg: string): never {
-//     throw new Error(errorMsg);
+// type Person = {
+//     firstName: string,
+//     lastName: string,
+//     age: number,
+//     jobTitle: string
 // }
 
-// throwError("Something went wrong serhat");
+// let instructor2: Person;
+// instructor2 = {
+//     firstName: 'serhat',
+//     lastName: 'Bulbul',
+//     age: 28,
+//     jobTitle: 'Web Developer'
+// }
 
-// let someThing3: void = undefined; //null ve undefined deger atanıyor
-// // let nothing3: never = undefined; // null ve undefined bile  degeri atanamıyor
-// console.log(someThing3)
-// // console.log(nothing3)
+// console.log(instructor2.age);
+
+// /////////////////////////////////////////////
+// // Data Types - Enum
+
+// enum Media {
+//     Newspaper = 10,
+//         Newsletter,
+//         Magazine,
+//         Book
+// }
+// console.log(Media.Magazine) //baslangic degerinden +1 koyarak sayıyor ve yazdırıyor
+// console.log(Media[13]); // --> Book
+
+// enum MediaP {
+//     Newspaper = "NEWSPAPER",
+//         Newsletter = "NEWSLETTER",
+//         Magazine = "MAGAZINE",
+//         Book = "BOOK"
+// }
+
+// console.log(MediaP.Newsletter); // --> NEWSLETTER
+// console.log(MediaP['Newspaper']); // --> NEWSPAPER
+
+// /////////////////////////////////////////////
+// // Data Types - Union
+
+// let codeee: string | number = "123";
+// console.log(codeee);
+
+// /////////////////////////////////////////////
+// // Data Types - Any
+
+// let someThing: any = "Hello";
+// console.log(someThing);
+// someThing = 33;
+// console.log(someThing);
+// someThing = {
+//     firstName: "Serhat",
+//     lastName: "Bulbul"
+// }
+// console.log(typeof (someThing));
+
+// let arr22: any[] = ["Serhat", 222, true];
+// console.log(arr22)
+
+
+// /////////////////////////////////////////////
+// // Data Types - Void
+
+// function sayHello(): void {
+//     console.log("Hello");
+// }
+
+// sayHello();
+
+// /////////////////////////////////////////////
+// // // Data Types - Never
+
+// // function throwError(errorMsg: string): never {
+// //     throw new Error(errorMsg);
+// // }
+
+// // throwError("Something went wrong serhat");
+
+// // let someThing3: void = undefined; //null ve undefined deger atanıyor
+// // // let nothing3: never = undefined; // null ve undefined bile  degeri atanamıyor
+// // console.log(someThing3)
+// // // console.log(nothing3)
 
 
 
-/////////////////////////////////////////////
-// Data Types - Inference
+// /////////////////////////////////////////////
+// // Data Types - Inference
 
-let sayac = 0;
+// let sayac = 0;
 
-console.log(typeof (sayac));
+// console.log(typeof (sayac));
 
-// function increment(counter: number){
+// // function increment(counter: number){
+// //     return counter++;
+// // }
+
+// function increment(counter: number): number { // geriye donus tipi de number olşdu
 //     return counter++;
 // }
 
-function increment(counter: number): number { // geriye donus tipi de number olşdu
-    return counter++;
-}
+// let a = "some text";
+// let b = 123;
 
-let a = "some text";
-let b = 123;
+// //a = b; //app.ts:196:1 - error TS2322: Type 'number' is not assignable to type 'string'
 
-//a = b; //app.ts:196:1 - error TS2322: Type 'number' is not assignable to type 'string'
-
-function sum(a: number, b: number) {
-    return a + b;
-}
-let total: number = sum(10, 12);
-// let total: string = sum(10, 12); //app.ts:202:5 - error TS2322: Type 'number' is not assignable to type 'string'.
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+// let total: number = sum(10, 12);
+// // let total: string = sum(10, 12); //app.ts:202:5 - error TS2322: Type 'number' is not assignable to type 'string'.
 
 
-/////////////////////////////////////////////
-// Data Types - Assertion
+// /////////////////////////////////////////////
+// // Data Types - Assertion
 
-let code: any = 123;
+// let code: any = 123;
 
-let empCode = < number > code;
-console.log(typeof (empCode));
+// let empCode = < number > code;
+// console.log(typeof (empCode));
 
-interface Employee {
-    name: string;
-    code: number;
-}
+// interface Employee {
+//     name: string;
+//     code: number;
+// }
 
-let employee12 = < Employee > {}
-console.log(typeof (employee12));
+// let employee12 = < Employee > {}
+// console.log(typeof (employee12));
 
-employee12.name = "Serhat";
-console.log(employee12);
+// employee12.name = "Serhat";
+// console.log(employee12);
 
 
 
 ///////////////////////////////////////////////////
 //TypeScript - Control Flow Statements
 ///////////////////////////////////////////////////
+
+// 1)  If Else Ternary Operator
+
+let x: number = 35;
+let y: number = 25;
+
+if (x > y){
+    console.log(`${x} buyuktur ${y}`);
+}else if (x < y){
+    console.log(`${x} kucuktur ${y}`);
+}else{
+    console.log(`${x} esit ${y}`);
+};
+
+(x>y) ? console.log(`${x} buyuktur ${y}`) : console.log(`${x} kucuk veya esit ${y}`);
+
+
+// 2)  Switch Case
+let day : number = 5;
+
+switch (day) {
+    case 0:
+        console.log("Pazar");
+        break;
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+    case 4:
+        console.log("Perşembe");
+        break;
+    case 5:
+        console.log("Cuma");
+        break;
+    case 6:
+        console.log("Cumartesi");
+        break;
+    default:
+        console.log("1-7 arasinda sayi gir");
+        break;
+}
+
+// 2)  For Loop
+
+for(let i = 0; i<2; i++) {
+    for(let j = 0; j < 3; j++) {
+        for(let k = 0; k < 3; k++) {
+            console.log(i, j, k);
+        }
+    }
+}
+
+let arr =[10,20,30,40,50];
+
+for(let item of arr) {   // for of
+    console.log(item)
+}
+
+let str = "Serhat Bülbül";
+for (let char of str) {
+    console.log(char);
+}
+
+for(let index in str){ // for in yapisi index degerini veriyor
+    console.log(index);
+}
+
+// 3) While Loop
+
+let counter = 5;
+while(counter < 5) {
+    console.log(counter);
+    counter++;
+
+    if(counter === 3) {
+        break;
+    }
+}
+
+// 4) Do While Loop
+
+do {
+    console.log(counter);
+    counter++;
+}while(counter<5)
+
